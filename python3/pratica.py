d88478b5011c2d9cf84f85559156fb3892cb5878
sans01hp <sans01hp@users.noreply.github.com>
2025-08-12 19:33:27 +0000

    adicionado mais explicacoes de comandos python3 e dois novos scripts usando o def e o lambda para receber uma lista de dominios

--- python3/pratica.py
+++ python3/pratica.py
@@ -1,8 +1,30 @@
-#!user/bin/env python3
 
 import requests
+import sys
 
-file = open('~/testes_python3_e_bash/python3/cores_ANSI')
+#file = open()
 
-for cor in file:
-    print(cor)
+#for cor in file:
+
+urls = sys.argv[1]
+
+status_code = [ 200, 403, 402, 301 ]
+# print(cor)
+def verifica_url(dominio, sc_valido):
+    
+    if "https://" not in dominio:
+        dominio = f"https://{dominio}"
+        
+    try:
+        r= requests.get(dominio)
+
+        if  r.status_code in sc_valido:
+             print("dominio ativo")
+             print(f"resposta={r.status_code}")
+    except:
+        print("dominio invalido ou nao listado")        
+        print(r.status_code)
+        
+         
+
+verifica_url(urls, status_code)   
